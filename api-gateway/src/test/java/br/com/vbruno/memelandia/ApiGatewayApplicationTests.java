7e54ea4eb841510af96cf94f13d17aba3ec0b14c
vagner.barbosa <vagner.barbosa@cwi.com.br>
2024-11-08 09:37:45 -0300

    Finalizei o projeto final

--- api-gateway/src/test/java/br/com/vbruno/memelandia/ApiGatewayApplicationTests.java
+++ api-gateway/src/test/java/br/com/vbruno/memelandia/ApiGatewayApplicationTests.java
@@ -1,10 +1,10 @@
-package br.com.ebac.memelandia;
+package br.com.vbruno.memelandia;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MemelandiaApplicationTests {
+class ApiGatewayApplicationTests {
 
 	@Test
 	void contextLoads() {
